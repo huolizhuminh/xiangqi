74ee3184ad0640745aabb978611db7e22ec96b70
minhui.zhu <minhui.zhu@mhs.local>
2017-03-23 17:00:18 +0800

    no message

--- engine/src/main/cpp/Position.cpp
+++ engine/src/main/cpp/Position.cpp
@@ -1,5 +1,12 @@
 //
 // Created by minhui.zhu on 2017/3/23.
 //
-
+#include <jni.h>
+#include <string>
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
 #include "Position.h"
+
+using namespace std;
+
